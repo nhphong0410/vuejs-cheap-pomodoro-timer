f013f9a0fd69ef10b8f7b97e8241cdb43583e8f2
nhphong0410 <nguyenhongphong281099@gmail.com>
2024-12-16 17:07:14 +0700

    Change timer buttons container to grid

--- src/App.vue
+++ src/App.vue
@@ -45,8 +45,10 @@ const isLongBreak = computed(() => activeTab.value === Tabs.LongBreak)
   width: fit-content;
   height: fit-content;
 }
+
 .type-buttons-container {
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
   gap: 1rem;
 }
 </style>
